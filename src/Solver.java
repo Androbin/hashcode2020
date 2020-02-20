02098049df89855fcee0ef53f4293b94b09c21dd
Anton Begehr <a.begehr@gmail.com>
2020-02-20 20:36:55 +0100

    solveMinSignup Solver

--- src/Solver.java
+++ src/Solver.java
@@ -60,4 +60,36 @@ public final class Solver {
 
         return result;
     }
+
+    public static List<Output> solveMinSignup(final List<Library> libraries, final int days) {
+        /*
+        final List<Library> registration = new ArrayList<>();
+        
+        registration.addAll(libraries);
+
+        for (int i = 0; i < days; i++) {
+            int day = 0;
+
+            for (int j = 0; j < libraries.size() - 1; j++) {
+                final Library a = registration.get(j);
+                final Library b = registration.get(j + 1);
+
+                final int scoreA1 = libraryInterest(a, days - day - a.signupTime);
+                final int scoreB1 = libraryInterest(b, days - day - a.signupTime - b.signupTime);
+
+                final int scoreA2 = libraryInterest(a, days - day - b.signupTime - a.signupTime);
+                final int scoreB2 = libraryInterest(b, days - day - b.signupTime);
+
+                if (scoreA2 + scoreB2 > scoreA1 + scoreB1) {
+                    Collections.swap(registration, j, j + 1);
+                    day += b.signupTime;
+                } else {
+                    day += a.signupTime;
+                }
+            }
+        }
+
+        final List<Output> plan = new ArrayList<>();
+        return plan;*/
+    }
 }
