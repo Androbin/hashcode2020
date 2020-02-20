5fc53753429edb34aac7786c298e98f54b363ffa
Anton Begehr <a.begehr@gmail.com>
2020-02-20 19:56:57 +0100

    impl toOutput()

--- src/Parser.java
+++ src/Parser.java
@@ -7,4 +7,15 @@ public final class Parser {
 	public static Input parse(final Stream<String> lineStream) {
 		return null;
     }
+
+	public static String toOutput(final List<Output> outputs) {
+		final StringBuilder result = new StringBuilder();
+		result.append(outputs.size()).append('\n');
+
+		for (final Output output : outputs) {
+			result.append(output.toString()).append('\n');
+		}
+
+		return result.toString();
+	}
 }
