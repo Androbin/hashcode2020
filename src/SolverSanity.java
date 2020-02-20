f290a527b5a6c79b085c013de12bc1875c81a84f
ske <ske@adorsys.de>
2020-02-20 20:47:39 +0100

    Fix SolverSanity

--- src/SolverSanity.java
+++ src/SolverSanity.java
@@ -6,24 +6,6 @@ public final class SolverSanity {
 	}
 
 	public static boolean sanity(final Input input, final List<Output> outputs) {
-		return SolverSanity.sanityScan(input, outputs);
-	}
-
-
-	public static boolean sanitySignup(final Input input, final List<Output> outputs) {
-		int days = input.days;
-		int maxSignupTs = 0;
-
-		for (final Output output : outputs) {
-			int signupTime = output.library.signupTime;
-			maxSignupTs += signupTime;
-		}
-
-		// can all libraries be signed-up in the set number of days?
-		return maxSignupTs <= days;
-	}
-
-	public static boolean sanityScan(final Input input, final List<Output> outputs) {
 		int days = input.days;
 		int maxSignupTs = 0;
 		int maxScanTs = 0;
