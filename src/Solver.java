7e1ed8836308d5bd7489f5562bf959822c67d01e
Anton Begehr <a.begehr@gmail.com>
2020-02-20 20:38:10 +0100

    solve greedy

--- src/Solver.java
+++ src/Solver.java
@@ -79,7 +79,7 @@ public final class Solver {
     }
 
     public static List<Output> solveMinSignup(final List<Library> libraries, final int days) {
-        
+
         final List<Library> registration = new ArrayList<>();
         
         registration.addAll(libraries);
